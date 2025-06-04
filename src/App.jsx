4ddbe9ac8f8c3a2a49c237bc8712957d6f6ade06
shreyaNeupane <shreyaneupane49@gmail.com>
2025-06-04 10:11:01 +0545

    learned useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Button from "./Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [name , setName] = useState("shreya");
+  //first type
+  useEffect(() => {
+    console.log("ewnders when page mounts or starts");
+  }, []);
+
+  //second type
+  // useEffect(()=>{
+  //   console.log("renders when page mounts or starts or when array dependency is triggered  ")
+  // },[count])
+
+  // //third type
+  // useEffect(()=>{
+  //   console.log("useEffect that triggers everytime")
+  // })
 
   return (
     <>
       <h1>{count}</h1>
-
       <button onClick={() => setCount(count + 1)}>+</button>
-    <h1>{name}</h1>
-    <button onClick={()=> setName("shreya neupane")}>changename</button>
     </>
   );
 }
