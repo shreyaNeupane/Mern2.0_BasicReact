1f8468e5c761b3680360b935e18241fbab8c66f9
shreyaNeupane <shreyaneupane49@gmail.com>
2025-06-04 08:40:31 +0545

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
-import './App.css'
-import Button from './Button'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Button from "./Button";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name , setName] = useState("shreya");
 
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        
-      </Routes>
-      </BrowserRouter>
+      <h1>{count}</h1>
+
+      <button onClick={() => setCount(count + 1)}>+</button>
+    <h1>{name}</h1>
+    <button onClick={()=> setName("shreya neupane")}>changename</button>
     </>
   );
 }
 
-export default App
+export default App;
